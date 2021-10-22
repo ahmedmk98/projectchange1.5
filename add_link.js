41ea937747fd50ae8ed3a9d708d4672055690c83
ahmedmk98 <92431012+ahmedmk98@users.noreply.github.com>
2021-10-22 09:09:09 +0300

    Add files via upload

--- add_link.js
+++ add_link.js
@@ -1,15 +1,16 @@
 
-
-const form = document.querySelector('#add-link-form');
+// this is page old please go to add_content.html
+/*const form = document.querySelector('#add-link-form');
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    db.collection('users').add({
+    db.collection('users').add(
+        {
         name: form.name.value,
         link: form.link.value
     });
     form.name.value = '';
     form.link.value = '';
-});
+});*/
 
